cb8567019fc5371c96b82b4b2be6d4d9ee815e8d
MegaBOOBLIK <megabooblik@gmail.com>
2023-03-29 17:47:54 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,15 +1,15 @@
 import time
+import pytest
 from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_butt_Add_In_Yor_Cart(browser):
+    browser.implicitly_wait(10)
     browser.get(link)
-    browser.find_element(By.XPATH, "//button[contains(@class,'btn-add-to-basket')]")
+    
+    assert browser.find_element(By.XPATH, "//button[contains(@class,'btn-add-to-basket')]")
     # browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
     # time.sleep(30)
     
-    button_name = browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket').text
-    assert button_name == f'Añadir al carrito', f'Vérifiez la langue ou corrigez la saisie :)'
-    if __name__ == "__main__"
-        pytest.main()    
+   
